8fc30c3cc0378ebc114423dcf6a4065cb08deeeb
liteblogging <litebloggingpro@gmail.com>
2025-05-16 23:36:35 +0530

    Refactor server.js to import build from @remix-run/dev/server-build for improved compatibility with Remix framework.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // This file is used by Vercel to run your Remix app
 
 import { createRequestHandler } from "@vercel/remix";
-import * as build from "./build/index.js";
+import * as build from "@remix-run/dev/server-build";
 
 export default createRequestHandler({
   build,
