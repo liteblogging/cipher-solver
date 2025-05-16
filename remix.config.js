afda7605f4baafd6cf1c9cbec14ce8e5eb9e7def
liteblogging <litebloggingpro@gmail.com>
2025-05-17 00:48:24 +0530

    Add Netlify deployment configuration

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
+module.exports = {
   appDirectory: `src`,
   ignoredRouteFiles: [`**/.*`],
+  serverModuleFormat: "cjs",
+  serverPlatform: "node",
+  serverDependenciesToBundle: "all",
+  future: {},
+  // Set the Netlify adapter for deployment
+  server: "./server.js",
 }
---
+++ server.js
@@ -0,0 +1,16 @@
+const { createRequestHandler } = require("@remix-run/netlify");
+const path = require("path");
+
+const BUILD_DIR = path.join(process.cwd(), "public/build");
+
+/**
+ * @type {import('@netlify/functions').HandlerEvent}
+ */
+exports.handler = async function (event, context) {
+  const requestHandler = createRequestHandler({
+    build: require(BUILD_DIR),
+    mode: process.env.NODE_ENV,
+  });
+
+  return requestHandler(event, context);
+}; 
