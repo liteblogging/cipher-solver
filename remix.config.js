fb09482ccddc6438362f97bdc98b117a9c1c6b2f
liteblogging <litebloggingpro@gmail.com>
2025-05-16 23:23:26 +0530

    Update remix.config.js to configure server settings and enable future features

--- remix.config.js
+++ remix.config.js
@@ -2,4 +2,15 @@
 export default {
   appDirectory: `src`,
   ignoredRouteFiles: [`**/.*`],
+  serverModuleFormat: "esm",
+  serverPlatform: "node",
+  publicPath: "/build/",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  future: {
+    v2_routeConvention: true,
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+    v2_meta: true,
+  },
 }
